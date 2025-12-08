8807bf7a2bafc05578dec64967c752eaa46dd570
Diego Branco <diegoh89@gmail.com>
2025-12-08 06:14:47 -0400

    Mi primera Integración continua (IC) con git action

--- RETO09/ATI2-2025-18443368-RETO09/demo/src/main/java/com/laboratorio4/demo/Ati2microservicioGreetingMicroserviceApplication.java
+++ RETO09/ATI2-2025-18443368-RETO09/demo/src/main/java/com/laboratorio4/demo/Ati2microservicioGreetingMicroserviceApplication.java
@@ -5,12 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @EnableDiscoveryClient
 @SpringBootApplication
 public class Ati2microservicioGreetingMicroserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Ati2microservicioGreetingMicroserviceApplication.class, args);
+        try {
+            SpringApplication.run(Ati2microservicioGreetingMicroserviceApplication.class, args);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
 	}
 
 }
